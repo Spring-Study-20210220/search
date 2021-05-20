5edb666d5eac98a86d783a51f724db6cce4f75f1
peachberry <peachberry0318@gmail.com>
2021-05-20 18:08:08 +0900

    feat(TypoDictionary) : 단어 검증 로직 테스트 작성

--- src/test/java/com/search/typodictionary/TypoDictionaryServiceTest.java
+++ src/test/java/com/search/typodictionary/TypoDictionaryServiceTest.java
@@ -1,11 +1,62 @@
 package com.search.typodictionary;
 
+import com.search.typodictionary.dto.TypoDictionaryInfo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class TypoDictionaryServiceTest {
     @InjectMocks
     private TypoDictionaryService typoDictionaryService;
+    @Mock
+    private TypoDictionaryRepository typoDictionaryRepository;
+    @Test
+    void 검색어교정테스트(){
+        //when
+        String[] keywords = {"스프링", "테스트"};
+        TypoDictionary typoDictionary1 = TypoDictionary.builder()
+                .typo("스프링")
+                .word("파이썬")
+                .build();
+        TypoDictionaryInfo typoDictionaryInfo1 = TypoDictionaryInfo.builder()
+                .from("스프링")
+                .to("파이썬")
+                .build();
+        TypoDictionary typoDictionary2 = TypoDictionary.builder()
+                .typo("테스트")
+                .word("토스트")
+                .build();
+        TypoDictionaryInfo typoDictionaryInfo2 = TypoDictionaryInfo.builder()
+                .from("테스트")
+                .to("토스트")
+                .build();
+        //given
+        given(typoDictionaryRepository.findByTypo(any()))
+                .willReturn(Optional.of(typoDictionary1))
+                .willReturn(Optional.of(typoDictionary2));
+        //then
+        List<TypoDictionaryInfo> result = typoDictionaryService.checkWords(keywords);
+//        for(TypoDictionaryInfo info : result) {
+//            assertThat(info.get).
+//        }
+//
+//        for(int i = 0; i < keywords.length; i++) {
+//
+//        }
+        assertThat(result).filteredOn("from","스프링")
+                .containsOnly(typoDictionaryInfo1);
+        assertThat(result).filteredOn("from","테스트")
+                .containsOnly(typoDictionaryInfo2);
+    }
 }
