3ea29fece345aac34b7e38cf853042af1353b5f3
peachberry <peachberry0318@gmail.com>
2021-05-20 18:00:32 +0900

    refactor(CensorWord) : CensoredWord -> CensorWord로패키지명 변경

--- src/main/java/com/search/censoredword/CensoredWordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.search.censoredword;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CensoredWordRepository extends JpaRepository<CensoredWord, Long> {
-}
--- src/main/java/com/search/censoredword/CensoredWordService.java
+++
@@ -1,5 +0,0 @@
-package com.search.censoredword;
-
-
-public class CensoredWordService {
-}
