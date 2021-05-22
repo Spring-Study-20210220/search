16557073a603a6c4282e8da59579e38d7435268d
Junsu <murane@naver.com>
2021-05-22 09:58:24 +0900

    refactor(CensorWordTest) : 검열 로직 수정에 따른 서비스 테스트 수정

--- src/test/java/com/search/censorword/CensorWordServiceTest.java
+++ src/test/java/com/search/censorword/CensorWordServiceTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-public class CensorWordServiceTest {
+class CensorWordServiceTest {
     @InjectMocks
     private CensorWordService censorWordService;
     @Mock
